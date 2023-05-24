2d15f7c46a4de2984e061d0caea8d3a74962ff6a
Ilshat Shamsetdinov <myheroisceleste@gmail.com>
2023-05-24 11:35:53 +0300

    prepare project

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,19 @@
 package hexlet.code;
+
+import io.javalin.Javalin;
+import io.javalin.core.JavalinConfig;
+
+
 public class App {
-    public static String greeting() {
-        return "hello world!";
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(JavalinConfig::enableDevLogging)
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start(8000);
+        return app;
+    }
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(8000);
     }
 }
 
