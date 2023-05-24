0421bbfca713eb64c375c02063987fd94c194214
Ilshat Shamsetdinov <myheroisceleste@gmail.com>
2023-05-24 17:46:35 +0300

    prepare project

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,12 +1,2 @@
 package hexlet.code;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-public class AppTest {
-    @Test
-    public void appTest() {
-        assertTrue(App.greeting().equals("hello world!"));
-    }
-}
